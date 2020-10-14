4ec6ed88826b661994dee200f3540d491b4ac692
Maria Shabalina <galimard.maria@gmail.com>
2020-10-14 09:53:57 +0800

    lesson 57

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import './spinner.css';
+
+const Spinner = () => {
+  return (
+      <div className="loadingio-spinner-double-ring-f4re9pi95j">
+          <div className="ldio-wy3s19ib5bo">
+              <div></div>
+              <div></div>
+              <div>
+                  <div></div>
+              </div>
+              <div>
+                  <div></div>
+              </div>
+          </div>
+      </div>
+  );
+};
+
+export default Spinner;
